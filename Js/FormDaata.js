a6c79c5a353a5289cec43ad5689ff171496162f3
Taxmanager-Bittu <taxmanager.bittu@gmail.com>
2023-09-14 15:22:10 +0530

    All Form Update

--- Js/FormDaata.js
+++ Js/FormDaata.js
@@ -1,12 +1,17 @@
-document.getElementById("contactFormf").addEventListener("submit", function (event) {
+const localUrl = "http://localhost:8080/sendcontectform";
+const OnlineUrl = "http://taxmanagercoin1-env.eba-yhppuvfm.ap-south-1.elasticbeanstalk.com/sendcontectform";
+
+function handleSubmit(event) {
     event.preventDefault();
 
+    // Find the form element that triggered the submit event
+    const form = event.target;
 
-    const name = document.getElementById("uNamef").value;
-    const email = document.getElementById("uEmailf").value;
-    const phone = document.getElementById("uPhoneNumberf").value;
-    const company = document.getElementById("uCompanyf").value;
-    const msg = document.getElementById("uMessagef").value;
+    const name = form.querySelector("#uName").value;
+    const email = form.querySelector("#uEmail").value;
+    const phone = form.querySelector("#uPhoneNumber").value;
+    const company = form.querySelector("#uCompany").value;
+    const msg = form.querySelector("#uMessage").value;
 
     const data = {
         Name: name,
@@ -16,15 +21,13 @@ document.getElementById("contactFormf").addEventListener("submit", function (eve
         Message: msg
     };
 
-
-    //fetch("http://taxmanagercoin1-env.eba-yhppuvfm.ap-south-1.elasticbeanstalk.com/sendcontectform", {
-    fetch("http://localhost:8080/sendcontectform", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(data)
-    })
+    fetch(`${localUrl}`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        })
         .then(response => response.json())
         .then(data => {
             // Handle the API response here
@@ -33,5 +36,7 @@ document.getElementById("contactFormf").addEventListener("submit", function (eve
         .catch(error => {
             console.log("Error:", error);
         });
+}
 
-});
+document.getElementById("contactFormf").addEventListener("submit", handleSubmit);
+document.getElementById("contactFormTop").addEventListener("submit", handleSubmit);
